aafff2abdbb3a52941914b430a6b4cd03485c699
Salvador Montiel <salvoperez51@gmail.com>
2024-08-13 21:00:17 -0600

    First commit

---
+++ HelloWorld/strings.py
@@ -0,0 +1,19 @@
+print("Today is a good day to learn python")
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("Hello" + " world")
+greeting = "Hello"
+name = "Peter"
+# If we want a space ,we can add that too.
+print(greeting + ' ' + name)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age = "2 years"
+print(age)
+print(type(age))
